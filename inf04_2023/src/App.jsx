bfcf6a872ab8a9133d43b57a1b80cd98962fc05b
Eksezis <official.eksezis@gmail.com>
2025-09-18 10:49:51 +0200

    Zad2 w procesie robienia

--- inf04_2023/src/App.jsx
+++ inf04_2023/src/App.jsx
@@ -2,11 +2,38 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Lista, setLista] = useState(["", "Komedia", "Obyczajowy", "Sensacyjny", "Horror"])
+  const [Tytul, setTytul] = useState()
+  const [Rodzaj, setRodzaj] = useState()
+
+
+  const onSubmit = (e) => {
+    console.log("tytul: " + Tytul + ";" + "rodzaj: " + Lista[Rodzaj])
+  }
+
+  const changeTytul = (e) => {
+    setTytul(e.target.value)
+  }
+
+  const changeRodzaj = (e) => {
+    setRodzaj(e.target.value)
+  }
 
   return (
     <>
-      
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Tytuł filmu</label>
+          <input className="form-control" type="text" onChange={changeTytul}/>
+          <label>Rodzaj</label>
+          <select className="form-control" onChange={changeRodzaj}>
+            {Lista.map((element, id)=>
+              <option value={Lista.indexOf} key={id}>{element}</option>
+            )}
+          </select>
+          <button type="submit" className="btn btn-primary">Dodaj</button>
+        </div>
+      </form>
     </>
   )
 }
